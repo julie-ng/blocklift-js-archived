f9487bf717772c22fc31f24f584d598a2ee654b9
Julie Ng <me@julie.io>
2019-12-21 21:28:00 +0100

    tests: add integration

---
+++ tests/containers.integration.spec.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const Blockport = require('../src/blockport')
+
+const serviceUrl = process.env.BLOB_SERVICE_SAS_URL
+/**
+ * Integration Tests against live Azure API
+ * Note: this test suite must be run in order
+ */
+describe ('Integration Tests: Containers', () => {
+	let bp
+	let runId
+
+	beforeAll(() => {
+		bp = new Blockport({
+			serviceUrl: serviceUrl
+		})
+
+		runId = 'integration-' + new Date().getMilliseconds()
+	})
+
+	it ('creates containers', (done) => {
+		return bp.createContainer(runId)
+			.then((data) => {
+				expect(data.containerName).toEqual(runId)
+				expect(data.statusText).toEqual('Created')
+				done()
+			})
+			.catch((err) => {
+				// TODO
+			})
+	})
+
+	it ('lists containers', (done) => {
+		return bp.listContainers()
+			.then((data) => {
+				expect(Array.isArray(data)).toBe(true)
+				expect(data.find((c) => c.Name === runId)).not.toBe(undefined)
+				done()
+			})
+			.catch((err) => {
+				// TODO
+			})
+	})
+
+	it ('deletes containers', (done) => {
+		return bp.deleteContainer(runId)
+			.then((data) => {
+				expect(data.containerName).toEqual(runId)
+				expect(data.statusText).toEqual('Accepted')
+				done()
+			})
+			.catch((err) => {
+				// TODO
+			})
+	})
+})
