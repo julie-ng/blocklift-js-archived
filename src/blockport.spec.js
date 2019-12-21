c9b10bea6ba42513511ce6d4cb0a01f2b9b4df98
Julie Ng <me@julie.io>
2019-12-21 09:53:42 +0100

    blockport: start tests

--- src/blockport.spec.js
+++ src/blockport.spec.js
@@ -1,8 +1,11 @@
+const axios = require('axios')
 const Blockport = require('./blockport')
 
 // Mock Service URL with SAS (expired)
 const testServiceUrl = "https://notarealaccount.blob.core.windows.net/?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2019-12-19T21:33:32Z&st=2019-12-19T13:33:32Z&spr=https&sig=jb9hodlbu9kMBA9KptT%2FArgRYcY3%2BCEs4fERQIWB8Y0%3D"
 
+const containersMocks = require('../mocks/containers')
+jest.mock('axios')
 
 describe ('Blockport', () => {
 	let bp
@@ -27,11 +30,26 @@ describe ('Blockport', () => {
 		})
 	})
 
-	describe ('createContainer()', () => {
-		xit ('can create a container', () => {
-			console.log(bp);
+	describe ('Containers', () => {
+		describe ('listContainers()', () => {
+			it ('can list containers', () => {
+				axios.get.mockResolvedValue({
+					data: containersMocks.list.xml
+				})
 
-			expect(bp.createContainer('test')).toEqual('foo')
+				return bp.listContainers().then((data)=> {
+					expect(data).toEqual(containersMocks.list.obj)
+				})
+			})
+		})
+
+		describe ('createContainer()', () => {
+			pending ('can create a container')
+			pending ('can delete a container')
+		})
+
+		describe ('deleteContainer()', () => {
+			pending ('can delete a container')
 		})
 	})
 })
