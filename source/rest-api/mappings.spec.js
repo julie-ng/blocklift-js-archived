23989c76433fed2d40a18004dbf7bed4d5424f75
Julie Ng <me@julie.io>
2019-12-23 17:00:58 +0100

    test: adjust mapping test

--- source/rest-api/mappings.spec.js
+++ source/rest-api/mappings.spec.js
@@ -1,29 +1,45 @@
+/* eslint-disable no-prototype-builtins */
 const mappings = require('./mappings')
 
 describe ('Mappings (for REST api)', () => {
 	describe ('Containers', () => {
 		const map = mappings.container
-		let keys = ['list', 'create', 'delete']
+		const keys = ['list', 'create', 'delete']
 
 		it ('has containers mappings', () => {
-			expect(mappings.container).not.toBe(undefined)
+			expect(mappings.hasOwnProperty('container')).toBe(true)
+		})
 
+		it ('mappings are defined correctly', () => {
 			keys.forEach((k) => {
 				expect(typeof map[k]).toEqual('function')
+				confirmRequiredProps(map[k])
 			})
 		})
 	})
 
 	describe ('Blobs', () => {
 		const map = mappings.blob
-		let keys = ['list', 'create', 'delete', 'createSnapshot', 'deleteSnapshot']
+		const keys = ['list', 'create', 'delete', 'createSnapshot', 'deleteSnapshot']
 
 		it ('has blobs mappings', () => {
-			expect(mappings.blob).not.toBe(undefined)
+			expect(mappings.hasOwnProperty('blob')).toBe(true)
+		})
 
+		it ('mappings are defined correctly', () => {
 			keys.forEach((k) => {
 				expect(typeof map[k]).toEqual('function')
+				confirmRequiredProps(map[k])
 			})
 		})
 	})
 })
+
+// --- helper ---
+
+function confirmRequiredProps (ref) {
+	let result = ref.call()
+	expect(result.hasOwnProperty('method')).toBe(true)
+	expect(result.hasOwnProperty('path')).toBe(true)
+	return
+}
