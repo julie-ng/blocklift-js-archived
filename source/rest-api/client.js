6988f6f5af7f8ee0ca8903b2b25ba1b3f6c56bab
Julie Ng <me@julie.io>
2019-12-25 22:45:51 +0100

    http-client: log all requests, duration

--- source/rest-api/client.js
+++ source/rest-api/client.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const chalk = require('chalk')
 const URL = require('./url')
 const transformXML = require('./xml-helper')
 
@@ -46,6 +47,18 @@ class HttpClient {
 			baseURL: this.host
 		})
 
+		// request interceptors
+		this.axios.interceptors.request.use(
+			addRequestStartTime,
+			(error) =>  Promise.reject(error)
+		)
+
+		// response interceptors
+		this.axios.interceptors.response.use(
+			logAllResponses,
+			logAllErrors
+		)
+
 		return this
 	}
 
@@ -68,11 +81,71 @@ class HttpClient {
 			url.append(this.sasToken)
 		}
 
-		return this.axios.request({
+		// TODO
+		// Add necessary authorization headers
+
+		let opts = {
 			method: api.method,
-			url: url.url
-		})
+			url: url.url,
+			data: api.data || ''
+		}
+
+		if (api.headers) {
+			opts = {
+				...opts,
+				headers: api.headers
+			}
+		}
+
+		return this.axios.request(opts)
 	}
 }
+
+const log = function (msg) {
+	console.log(chalk.blue(`[HttpClient] `) + msg)
+}
+
+function addRequestStartTime (config) {
+	config.metadata = { startTime: new Date() }
+	return config
+}
+
+function logAllResponses (response) {
+	let metadata = response.config.metadata
+	metadata.endTime = new Date()
+	response.duration = metadata.endTime - metadata.startTime
+
+	let req = response.request
+	let server = `${req.agent.protocol}//${req.connection.servername}`
+
+	log(
+		req.method.toUpperCase()
+		+ ' '
+		+ server
+		+ req.path
+		+ ' ' + chalk.green(response.status) + ', ' + `${response.duration} ms`
+	)
+	return response
+}
+
+function logAllErrors (error) {
+	let metadata = error.config.metadata
+	metadata.endTime = new Date()
+	error.duration = metadata.endTime - metadata.startTime
+
+	console.log(chalk.bgRed.white(' HTTP Error ') + ' ' + chalk.red('[HttpClient] '))
+
+	const obj = {
+		status: error.response.status,
+		statusText: error.response.statusText,
+		request: {
+			method: error.request.method,
+			path: error.request.path
+		}
+	}
+	console.log(obj)
+	return Promise.reject(error)
+}
+
 
 module.exports = HttpClient
