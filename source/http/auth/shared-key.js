1ce03d536238c185c25a1090593a9396412ce708
Julie Ng <me@julie.io>
2019-12-29 10:57:13 +0100

    shared-key: use canonicalized, still missing tests

--- source/http/auth/shared-key.js
+++ source/http/auth/shared-key.js
@@ -1,61 +1,87 @@
-let template = function (VERB) {
+const canonicalized = require('./canonicalized')
+
+/**
+ * Generates signature strings for authorization
+ * against Azure Storage API when using Access keys.
+ * For details, see [Azure Documentation](https://docs.microsoft.com/en-gb/rest/api/storageservices/authorize-with-shared-key)
+ *
+ * ```
+ * Authorization: SharedKey myaccount:ctzMq410TV3wS7upTBcunJTDLEJwMAZuFPfr0mrrA08=
+ * ```
+ */
+class SharedKey {
+	/**
+	 *
+	 * @param {String} account - name of Azure Storage account
+	 */
+	constructor (account) {
+		this.account = account
+	}
+
+	/**
+	 * Generates signature for `Authorization` header
+	 *
+	 * @param {String} method - HTTP verb, e.g. `GET`, `PUT`, etc.
+	 * @param {Object} headers - HTTP headers
+	 * @param {String} url - full url incl. `https://`
+	 * @return {String} - signature
+	 */
+	generate (method, headers, url) {
+		const signature = _signatureTemplate(method, headers, url)
+		return `Authorization: SharedKey ${this.account}:${signature}`
+	}
+}
+
+// --- Helpers ---
+
+const _signatureHeaders = [
+	'Content-Encoding',
+	'Content-Language',
+	'Content-Length',
+	'Content-MD5',
+	'Content-Type',
+	'Date',
+	'If-Modified-Since',
+	'If-Match',
+	'If-None-Match',
+	'If-Unmodified-Since',
+	'Range',
+]
+
+const _signatureTemplate = function (method, headers, url) {
 
 	// This part of signature is based on headers.
 	// The following are optional ones that can be empty.
 	// Order matters
 	// "Where there is no header value, the new-line character only is specified."
-	const params = {
-		'Content-Encoding': '',
-		'Content-Language': '',
-		'Content-Length': '',
-		'Content-MD5': '',
-		'Content-Type': '',
-		'Date': '',
-		'If-Modified-Since': '',
-		'If-Match': '',
-		'If-None-Match': '',
-		'If-Unmodified-Since': '',
-		'Range': ''
-	}
 
-	return `
-		VERB + "\n" +
-							Content-Encoding + "\n" +
-							Content-Language + "\n" +
-							Content-Length + "\n" +
-							Content-MD5 + "\n" +
-							Content-Type + "\n" +
-							Date + "\n" +
-							If-Modified-Since + "\n" +
-							If-Match + "\n" +
-							If-None-Match + "\n" +
-							If-Unmodified-Since + "\n" +
-							Range + "\n" +
-							CanonicalizedHeaders +
-							CanonicalizedResource;
-	`
-}
+	let result = method.toUpperCase() + '\n'
+	// result += _appendSignature(headers, 'Content-Encoding')
+	// result += _appendSignature(headers, 'Content-Language')
+	// result += _appendSignature(headers, 'Content-Length')
+	// result += _appendSignature(headers, 'Content-MD5')
+	// result += _appendSignature(headers, 'Content-Type')
+	// result += _appendSignature(headers, 'Date')
+	// result += _appendSignature(headers, 'If-Modified-Since')
+	// result += _appendSignature(headers, 'If-Match')
+	// result += _appendSignature(headers, 'If-None-Match')
+	// result += _appendSignature(headers, 'If-Unmodified-Since')
+	// result += _appendSignature(headers, 'Range')
 
-// See: https://docs.microsoft.com/en-gb/rest/api/storageservices/authorize-with-shared-key#constructing-the-canonicalized-headers-string
-const generateCanonicalizedHeaders = function (headers = {}) {
-	// Retrieve all headers for the resource that begin with x-ms-, including the x-ms-date header.
+	_signatureHeaders.forEach((h) => {
+		result += _appendSignature(headers, h)
+	})
+
+	result += canonicalized.header(headers)
+	result += canonicalized.resource(url)
+	return result
 }
 
 
-class SharedKey {
-	/**
-	 *
-	 * @param {String} verb - HTTP verb, e.g. `GET`, `PUT`, `DELETE`, etc.
-	 * @param {Object} headers={}
-	 */
-	constructor (verb) {
-
-	}
-
-
-	generate () {
-
-	}
+const _appendSignature = function (params, prop) {
+	return (params[prop] === '')
+		? '\n'
+		: params[prop] + '\n'
 }
 
 module.exports = SharedKey
